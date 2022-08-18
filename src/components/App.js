d767e6b1cdd875337eda9df94a12e19018e6f1df
ire-rec <i.recavarren@gmail.com>
2022-08-18 19:46:30 +0200

    comment text Monday Haters

--- src/components/App.js
+++ src/components/App.js
@@ -385,7 +385,24 @@ function App() {
           </fieldset>
         </form>
       </main>
-
+      {/* <footer className="footer">
+        <div className="footer__text">
+          <p>
+            Monday haters <small>©2022</small>
+          </p>
+        </div>
+        <div className="footer__logo">
+          <a href="./index.html#">
+            {' '}
+            <img
+              className="footer__logo--img"
+              src={logo}
+              alt="logo"
+              title="logo"
+            />
+          </a>
+        </div>
+      </footer> */}
       <script src="./assets/js/main.js"></script>
     </div>
   );
