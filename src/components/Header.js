320a683ccf3514c4abf3206474b934e2b0a426f5
LunaRocio <r.lunamerchan@gmail.com>
2022-09-05 12:25:14 +0200

    add styles imports II

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import "../styles/layout/Header.scss";
 
 const Header = (props) => {
   return (
--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-
+import "../styles/layout/Main.scss";
 const Landing = () => {
   return (
     <main className="main">
